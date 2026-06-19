3d5fa5126edc83f73b80fd6c87b47e074d6ae89b
testingudemy123-mogi <testingudemy123@gmail.com>
2026-06-19 22:26:09 +0530

    Update nse_download_email.py

--- nse_download_email.py
+++ nse_download_email.py
@@ -36,6 +36,7 @@ try:
     from selenium.webdriver.common.by import By
     from selenium.webdriver.support.ui import WebDriverWait
     from selenium.webdriver.support import expected_conditions as EC
+    from selenium.webdriver import ChromeOptions
 
 except Exception as e:
     print("IMPORT ERROR:")
